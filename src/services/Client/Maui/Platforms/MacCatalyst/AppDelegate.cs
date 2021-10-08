768abcaea3da627f9c1fca95da5d62ef4ea24a22
Louis <louis@arkiaconsulting.com>
2021-10-08 22:32:14 +0200

    Create projects Api and Maui

---
+++ src/services/Client/Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Booliba.Client.Maui;
+using Foundation;
+using Microsoft.Maui;
+
+namespace Booliba.Client.Maui.Platforms.MacCatalyst
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
