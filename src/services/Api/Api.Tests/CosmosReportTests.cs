5c2643a1cdcb4d8373e2e617b377351ee8f6c724
Louis <louis@arkiaconsulting.com>
2021-10-09 13:34:51 +0200

    Setup test project

---
+++ src/services/Api/Api.Tests/CosmosReportTests.cs
@@ -0,0 +1,49 @@
+// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Booliba.Tests.Fixtures;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Booliba.Tests
+{
+    [Trait("Category", "Component")]
+    public class CosmosReportTests : IAsyncLifetime
+    {
+        private CosmosClient CosmosClient => _host.Services.GetRequiredService<CosmosClient>();
+        private Container ReportsContainer => _container ?? throw new InvalidOperationException("Wrong initialization");
+
+        private readonly IHost _host;
+        private readonly string _databaseName = Guid.NewGuid().ToString();
+        private Container? _container;
+
+        public CosmosReportTests() => _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services => services.AddSingleton(CosmosFixture.EmulatorClient))
+                .Build();
+
+        [Fact]
+        public async Task Test1()
+        {
+
+        }
+
+        #region IAsyncLifetime
+
+        async Task IAsyncLifetime.InitializeAsync()
+        {
+            Database db = await CosmosClient.CreateDatabaseIfNotExistsAsync(_databaseName, 10000);
+            _container = await CosmosFixture.CreateReportsContainer(db);
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await CosmosClient.GetDatabase(_databaseName).DeleteAsync();
+        }
+
+        #endregion
+    }
+}
---
+++ src/services/Api/Database/Cosmos/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Azure.Core;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCosmos(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(sp =>
+            {
+                var credential = sp.GetRequiredService<TokenCredential>();
+
+                return new CosmosClient(configuration["Cosmos:Endpoint"], credential);
+            });
+
+            return services;
+        }
+    }
+}
