7ff3c21b0aaaf2e2a2fdf941b7ba70848ef7caaf
Louis <louis@arkiaconsulting.com>
2021-10-09 13:34:51 +0200

    Setup test project

---
+++ src/services/Api/Api.Tests/Fixtures/CosmosFixture.cs
@@ -0,0 +1,33 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using System;
+using System.Threading.Tasks;
+
+namespace Booliba.Tests.Fixtures
+{
+    internal class CosmosFixture : IDisposable
+    {
+        private static readonly CosmosClient CosmosClient;
+
+        public static CosmosClient EmulatorClient => CosmosClient;
+
+        static CosmosFixture() => CosmosClient = new CosmosClientBuilder(
+                "https://localhost:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+            .WithSerializerOptions(new() { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase })
+            .Build();
+
+        public void Dispose() => ((IDisposable)CosmosClient).Dispose();
+
+        internal static async Task<Container> CreateReportsContainer(Database database)
+        {
+            return (await database.CreateContainerIfNotExistsAsync(new ContainerProperties
+            {
+                Id = "reports",
+                PartitionKeyPath = "/userId"
+            })).Container;
+        }
+    }
+}
--- src/services/Api/Api/Program.cs
+++ src/services/Api/Api/Program.cs
@@ -1,3 +1,5 @@
+// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
