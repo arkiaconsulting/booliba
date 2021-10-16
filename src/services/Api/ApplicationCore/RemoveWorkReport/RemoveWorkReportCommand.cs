2503c1ee333c607abb5e7706d3b9bd5543b32d0b
Louis <louis@arkiaconsulting.com>
2021-10-16 16:25:02 +0200

    Remove a work report logic

---
+++ src/services/Api/ApplicationCore/RemoveWorkReport/RemoveWorkReportCommand.cs
@@ -0,0 +1,33 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Booliba.ApplicationCore.AddReport;
+using Booliba.ApplicationCore.Ports;
+using MediatR;
+
+namespace Booliba.ApplicationCore.RemoveWorkReport
+{
+    public record RemoveWorkReportCommand(Guid WorkReportId) : IRequest;
+
+    internal class RemoveDaysCommandHandler : IRequestHandler<RemoveWorkReportCommand>
+    {
+        private readonly IEventBus _eventBus;
+        private readonly IRepository _repository;
+
+        public RemoveDaysCommandHandler(IEventBus eventBus, IRepository repository)
+        {
+            _eventBus = eventBus;
+            _repository = repository;
+        }
+
+        async Task<Unit> IRequestHandler<RemoveWorkReportCommand, Unit>.Handle(RemoveWorkReportCommand request, CancellationToken cancellationToken)
+        {
+            var events = await _repository.Load(request.WorkReportId, cancellationToken);
+            if (events.OfType<ReportAdded>().Where(e => e.WorkReportId == request.WorkReportId).Any())
+            {
+                await _eventBus.Publish(new WorkReportRemoved(request.WorkReportId), cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
