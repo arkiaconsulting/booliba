111c0d597229c286d764da802c46ea06fe676dc6
Louis <louis@arkiaconsulting.com>
2021-10-16 16:25:02 +0200

    Remove a work report logic

---
+++ src/services/Api/Api.Tests/Domain/RemoveWorkReportShould.cs
@@ -0,0 +1,57 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using AutoFixture;
+using Booliba.ApplicationCore.AddReport;
+using Booliba.ApplicationCore.RemoveWorkReport;
+using Booliba.Tests.Fixtures;
+using FluentAssertions;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Booliba.Tests.Domain
+{
+    [Trait("Category", "Unit")]
+    public class RemoveWorkReportShould : IClassFixture<TestContext>
+    {
+        private readonly TestContext _context;
+
+        public RemoveWorkReportShould(TestContext context) => _context = context;
+
+        [Theory(DisplayName = "Effectively remove an existing work report"), BoolibaInlineAutoData]
+        public async Task Test01(RemoveWorkReportCommand command, Fixture fixture)
+        {
+            _ = AddWorkReport(command.WorkReportId, fixture);
+            await _context.Sut.Send(command, CancellationToken.None);
+
+            _context.Events.OfType<WorkReportRemoved>().Where(e => e.WorkReportId == command.WorkReportId)
+                .Should().ContainSingle();
+        }
+
+        [Theory(DisplayName = "Not remove a non existing report"), BoolibaInlineAutoData]
+        public async Task Test02(RemoveWorkReportCommand command)
+        {
+            await _context.Sut.Send(command, CancellationToken.None);
+
+            _context.Events.OfType<WorkReportRemoved>().Where(e => e.WorkReportId == command.WorkReportId)
+                .Should().BeEmpty();
+        }
+
+        #region Private
+
+        private ReportAdded AddWorkReport(Guid workReportId, Fixture fixture)
+        {
+            var workReportAddedEvent = fixture.Build<ReportAdded>()
+                .With(c => c.WorkReportId, workReportId)
+                .Create();
+
+            _context.Events.Add(workReportAddedEvent);
+
+            return workReportAddedEvent;
+        }
+
+        #endregion
+    }
+}
