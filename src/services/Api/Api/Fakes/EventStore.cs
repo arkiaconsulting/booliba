b9f6671032f509beb47781517d6cd0a0de6c2de7
Louis <louis@arkiaconsulting.com>
2021-10-24 15:28:03 +0200

    Expose business logic using the Api controller

---
+++ src/services/Api/Api/Fakes/EventStore.cs
@@ -0,0 +1,66 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Booliba.Api.Fakes;
+using Booliba.ApplicationCore.Ports;
+using Booliba.ApplicationCore.SendReport;
+
+namespace Booliba.Api.Fakes
+{
+    internal class InMemoryEventStore : IEventStore
+    {
+        private readonly ICollection<WorkReportEvent> _events = new List<WorkReportEvent>();
+        private readonly ILogger<InMemoryEventStore> _logger;
+
+        public InMemoryEventStore(ILogger<InMemoryEventStore> logger) => _logger = logger;
+
+        Task<IEnumerable<WorkReportEvent>> IEventStore.Load(Guid workReportId, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Loading events from the event store");
+
+            return Task.FromResult(
+                _events
+                .Where(e => e.WorkReportId == workReportId)
+                .ToList()
+                .AsEnumerable()
+                );
+        }
+
+        Task IEventStore.Save(IEnumerable<WorkReportEvent> events, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Saving {EventCount} events to the event store", events.Count());
+
+            events.ToList().ForEach(e => _events.Add(e));
+
+            return Task.CompletedTask;
+        }
+    }
+
+    internal class NullEmailNotifier : IEmailNotifier
+    {
+        private readonly ILogger<NullEmailNotifier> _logger;
+
+        public NullEmailNotifier(ILogger<NullEmailNotifier> logger) => _logger = logger;
+
+        Task IEmailNotifier.Send(EmailMessage emailMessage, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Notifying {RecipientCount} recipients of work report {WorkReportId}", emailMessage.EmailAddresses.Length, emailMessage.WorkReportId);
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static class EventStoreExtensions
+    {
+        public static IServiceCollection AddInMemoryEventStore(this IServiceCollection services) =>
+            services.AddSingleton<IEventStore, InMemoryEventStore>();
+    }
+
+    internal static class EmailNotifierExtensions
+    {
+        public static IServiceCollection AddNullEmailNotifier(this IServiceCollection services) =>
+            services.AddTransient<IEmailNotifier, NullEmailNotifier>();
+    }
+}
---
+++ src/services/Api/Api/Infrastructure/DateOnlyConverter.cs
@@ -0,0 +1,16 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Booliba.Api.Infrastructure
+{
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            DateOnly.Parse(reader.GetString()!);
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) =>
+            writer.WriteStringValue(value.ToString());
+    }
+}
