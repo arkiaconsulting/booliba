17e11fe0eceb16aefc374e413fe81afcaacb1ad6
Louis <louis@arkiaconsulting.com>
2021-10-30 10:28:39 +0200

    Implement in-memory event handlers in order to update the query side

--- src/services/Api/Api/Fakes/InMemoryProjection.cs
+++ src/services/Api/Api/Fakes/InMemoryProjection.cs
@@ -1,9 +1,11 @@
 ﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
 
+using Booliba.Api.Fakes;
 using Booliba.QuerySide;
-using Booliba.Tests.Fixtures;
+using MediatR;
+using System.Reflection;
 
-namespace Booliba.Tests.Fixtures
+namespace Booliba.Api.Fakes
 {
     internal class InMemoryProjection : IWorkReportProjection
     {
@@ -47,6 +49,48 @@ namespace Booliba.Tests.Fixtures
 
         #endregion
     }
+
+    internal class InMemoryEventHandler :
+        INotificationHandler<DaysAddedNotification>,
+        INotificationHandler<ReportAddedNotification>,
+        INotificationHandler<DaysRemovedNotification>,
+        INotificationHandler<WorkReportRemovedNotification>,
+        INotificationHandler<WorkReportSentNotification>
+    {
+        private readonly ProjectionService _projectionService;
+
+        public InMemoryEventHandler(ProjectionService projectionService) => _projectionService = projectionService;
+
+        Task INotificationHandler<DaysAddedNotification>.Handle(DaysAddedNotification notification, CancellationToken cancellationToken) =>
+            _projectionService.AddDays(
+                notification.Event.WorkReportId,
+                notification.Event.Days.ToArray(),
+                cancellationToken);
+
+        Task INotificationHandler<ReportAddedNotification>.Handle(ReportAddedNotification notification, CancellationToken cancellationToken) =>
+            _projectionService.CreateWorkReport(
+                notification.Event.WorkReportId,
+                notification.Event.WorkReportName,
+                notification.Event.Days.ToArray(),
+                cancellationToken);
+
+        Task INotificationHandler<DaysRemovedNotification>.Handle(DaysRemovedNotification notification, CancellationToken cancellationToken) =>
+            _projectionService.RemoveDays(
+                notification.Event.WorkReportId,
+                notification.Event.Days.ToArray(),
+                cancellationToken);
+
+        Task INotificationHandler<WorkReportRemovedNotification>.Handle(WorkReportRemovedNotification notification, CancellationToken cancellationToken) =>
+            _projectionService.Remove(
+                notification.Event.WorkReportId,
+                cancellationToken);
+
+        Task INotificationHandler<WorkReportSentNotification>.Handle(WorkReportSentNotification notification, CancellationToken cancellationToken) =>
+            _projectionService.AddRecipients(
+                notification.Event.WorkReportId,
+                notification.Event.EmailAddresses,
+                cancellationToken);
+    }
 }
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -54,6 +98,7 @@ namespace Microsoft.Extensions.DependencyInjection
     internal static partial class ConfigurationExtensions
     {
         public static IServiceCollection AddInMemoryProjection(this IServiceCollection services) =>
-            services.AddSingleton<IWorkReportProjection, InMemoryProjection>();
+            services.AddSingleton<IWorkReportProjection, InMemoryProjection>()
+            .AddMediatR(Assembly.GetExecutingAssembly());
     }
 }
