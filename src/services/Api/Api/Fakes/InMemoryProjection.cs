3821edbe773d1c501b8e1cec60bd58234dad32f7
Louis <louis@arkiaconsulting.com>
2021-10-30 09:59:24 +0200

    Implement an in-memory work report projection

---
+++ src/services/Api/Api/Fakes/InMemoryProjection.cs
@@ -0,0 +1,59 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using Booliba.QuerySide;
+using Booliba.Tests.Fixtures;
+
+namespace Booliba.Tests.Fixtures
+{
+    internal class InMemoryProjection : IWorkReportProjection
+    {
+        public ICollection<WorkReportEntity> WorkReports => _workReportEntities;
+
+        private readonly ICollection<WorkReportEntity> _workReportEntities = new List<WorkReportEntity>();
+
+        #region IWorkReportProjection
+
+        Task<WorkReportEntity[]> IWorkReportProjection.List(CancellationToken cancellationToken) =>
+            Task.FromResult(_workReportEntities.ToArray());
+
+        Task<WorkReportEntity?> IWorkReportProjection.Get(Guid workReportId, CancellationToken cancellationToken) =>
+            Task.FromResult(
+                _workReportEntities
+                .SingleOrDefault(wr => wr.Id == workReportId)
+            );
+        Task IWorkReportProjection.Add(WorkReportEntity entity, CancellationToken cancellationToken)
+        {
+            _workReportEntities.Add(entity);
+
+            return Task.CompletedTask;
+        }
+
+        Task IWorkReportProjection.Update(WorkReportEntity entity, CancellationToken cancellationToken)
+        {
+            var existingEntity = _workReportEntities.Single(e => e.Id == entity.Id);
+            _workReportEntities.Remove(existingEntity);
+            _workReportEntities.Add(entity);
+
+            return Task.CompletedTask;
+        }
+
+        Task IWorkReportProjection.Delete(Guid workReportId, CancellationToken cancellationToken)
+        {
+            var existingEntity = _workReportEntities.Single(e => e.Id == workReportId);
+            _workReportEntities.Remove(existingEntity);
+
+            return Task.CompletedTask;
+        }
+
+        #endregion
+    }
+}
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static partial class ConfigurationExtensions
+    {
+        public static IServiceCollection AddInMemoryProjection(this IServiceCollection services) =>
+            services.AddSingleton<IWorkReportProjection, InMemoryProjection>();
+    }
+}
