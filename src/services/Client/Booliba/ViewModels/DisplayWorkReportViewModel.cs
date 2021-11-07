8f9144527fb1b431291727fd243021f4844b9034
Louis <louis@arkiaconsulting.com>
2021-11-07 09:48:27 +0100

    Display work reports

---
+++ src/services/Client/Booliba/ViewModels/DisplayWorkReportViewModel.cs
@@ -0,0 +1,27 @@
+﻿// This code is under Copyright (C) 2021 of Arkia Consulting SAS all right reserved
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Booliba.ViewModels
+{
+    internal class DisplayWorkReportViewModel
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public IEnumerable<IGrouping<int, DateOnly>> DaysPerYear { get; private set; }
+        public int TotalDayCount { get; private set; }
+
+        public DisplayWorkReportViewModel(
+            Guid id,
+            string name,
+            IEnumerable<DateOnly> days)
+        {
+            Id = id;
+            Name = name;
+            DaysPerYear = days.GroupBy(d => d.Year);
+            TotalDayCount = days.Count();
+        }
+    }
+}
