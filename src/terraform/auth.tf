504f6660c2c41b7de50fdfbfb721de94873decde
Louis <louis@arkiaconsulting.com>
2021-12-29 18:01:23 +0100

    Add additional autorization scopes

--- src/terraform/auth.tf
+++ src/terraform/auth.tf
@@ -1,6 +1,6 @@
 resource "akcauth_authorization_code_client" "booliba" {
   client_id = "booliba"
   client_name = "Booliba"
-  allowed_scopes = []
-  redirect_uris = ["https://localhost:9999/callback"]
+  allowed_scopes = [ "openid", "profile", "booliba.basic" ]
+  redirect_uris = ["io.identitymodel.native://callback"]
 }
